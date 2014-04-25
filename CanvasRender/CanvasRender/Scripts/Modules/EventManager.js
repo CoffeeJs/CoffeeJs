0ad422471937012a33b9efd679f38bef29fcc9c3
Soulskater <gmeszaros007@gmail.com>
2014-04-25 19:58:10 +0200

    Added CanvasRender project

---
+++ CanvasRender/CanvasRender/Scripts/Modules/EventManager.js
@@ -0,0 +1,71 @@
+﻿/***************************************************************
+*
+*   EventManager
+*
+***************************************************************/
+var EventManager;
+(EventManager = function () {
+}).prototype = {
+    subscribe: function (type, method, scope, context) {
+        var listeners, handlers, scope;
+        if (!(listeners = this.listeners)) {
+            listeners = this.listeners = {};
+        }
+        if (!(handlers = listeners[type])) {
+            handlers = listeners[type] = [];
+        }
+        scope = (scope ? scope : window);
+        handlers.push({
+            method: method,
+            scope: scope,
+            context: (context ? context : scope)
+        });
+    },
+    unsubscribe: function (type) {
+        listeners.splice(listeners.indexOf(type), 1);
+    },
+    trigger: function (type, data, context) {
+        var listeners, handlers, i, n, handler, scope;
+        if (!(listeners = this.listeners)) {
+            return;
+        }
+        if (!(handlers = listeners[type])) {
+            return;
+        }
+        for (i = 0, n = handlers.length; i < n; i++) {
+            handler = handlers[i];
+            if (typeof (context) !== "undefined" && context !== handler.context) continue;
+            if (handler.method.call(
+                handler.scope, data, type
+            ) === false) {
+                return false;
+            }
+        }
+        return true;
+    }
+};
+
+function Observable(initial) {
+
+    var update = function (value) {
+        if (arguments.length > 0) {
+            update._value = arguments[0];
+            for (var i = 0; i < update._subscribers.length; i++) {
+                update._subscribers[i](update._value);
+            }
+        }
+        return update._value;
+    };
+
+    update._value = initial;
+    update._subscribers = [];
+    update.subscribe = function (callback) {
+        update._subscribers.push(callback);
+    };
+
+    update.unsubscribe = function (callback) {
+        update._subscribers.splice(update._subscribers.indexOf(callback), 1);
+    };
+
+    return update;
+}
---
+++ CanvasRender/CanvasRender/Scripts/Modules/UI/Render/Render.js
@@ -0,0 +1,132 @@
+﻿var RenderCanvas = function (buffers) {
+    var self = this;
+
+    var _FPS = 50;
+    var _objects = [];
+    var _buffers = buffers;
+    var _events = { onInvalidate: "onInvalidate" };
+    var eventManager = new EventManager();
+
+    this.getContext = (function () {
+        var aktBuffer = 0;
+
+        return function (buffering) {
+            if (buffering === true) {
+                _buffers[1 - aktBuffer].style.visibility = 'hidden';
+                _buffers[aktBuffer].style.visibility = 'visible';
+
+                aktBuffer = 1 - aktBuffer;
+            }
+
+            return _buffers[aktBuffer].getContext("2d");
+        }
+    }());
+
+    this.invalidate = function () {
+        var ctx = self.getContext(true);
+        ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height);
+
+        for (var i = 0, l = _objects.length; i < l; i++) {
+            _objects[i].draw(ctx);
+        }
+        eventManager.trigger(_events.onInvalidate, {
+            context: ctx,
+            fps: _FPS
+        });
+    };
+
+    this.onInvalidate = function (handler) {
+        eventManager.subscribe(_events.onInvalidate, handler);
+    }
+
+    this.addObject = function (obj) {
+        if (!(obj instanceof CanvasObject)) {
+            console.log("An object on the canvas should be inherited from CanvasObject!");
+            return;
+        }
+        _objects.push(obj);
+        _invalid = true;
+    };
+    this.renderByHand = function () {
+        for (var i = 0; i < 100; i++) {
+            self.invalidate();
+            //Utils.sleep(1);
+        }
+    }
+
+    this.render = function () {
+        setInterval(function () {
+            self.invalidate();
+        }, 1000 / _FPS);
+    }
+}
+
+function Point(x, y) {
+    return {
+        x: x || 0,
+        y: y || 0,
+        equalsTo: function (p) {
+            return x === p.x && y === p.y;
+        }
+    };
+}
+
+function Rect(x, y, width, height) {
+    return {
+        x: x,
+        y: y,
+        width: width,
+        height: height
+    }
+}
+
+function TransformationObject() {
+    var self = this;
+
+    this.angle = 0;
+
+    this.rotateFunction = null;
+    this.scaleFunction = null;
+
+    this.rotate = function (deg) {
+        self.rotateFunction = function (ctx, obj) {
+            var o = obj.getCenter();
+            ctx.translate(o.x, o.y);
+            ctx.rotate(Utils.convertToRad(deg));
+            ctx.translate(-o.x, -o.y);
+            self.angle = deg;
+        };
+    }
+
+    this.scale = function (width, height) {
+        self.scaleFunction = function (ctx, obj) {
+            var o = obj.getCenter();
+            ctx.translate(o.x, o.y);
+            ctx.scale(width, height);
+            ctx.translate(-o.x, -o.y);
+        }
+    }
+}
+
+function CanvasObject() {
+
+    this.position = Point(0, 0);
+    this.width = 0;
+    this.height = 0;
+
+    this.getCenter = function () {
+
+    }
+
+    this.draw = function (ctx) {
+    }
+
+    this.destroy = function (ctx) {
+
+    }
+
+
+}
+
+CanvasObject.prototype = new TransformationObject();
+CanvasObject.prototype.constructor = CanvasObject;
