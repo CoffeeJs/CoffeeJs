b1f45f9ef665ba127586590014cd2d49867c80ac
Soulskater <gmeszaros007@gmail.com>
2014-04-30 19:42:31 +0200

    Optimized render

--- CanvasRender/CanvasRender/Scripts/Modules/UI/Render/Render.js
+++ CanvasRender/CanvasRender/Scripts/Modules/UI/Render/Render.js
@@ -37,42 +37,85 @@
     }
 
     var createLayers = function () {
-        _layers = [];
-        $("canvas", "#viewport").remove();
         $("#viewport").width(width);
         $("#viewport").height(height);
-        for (var i = 0, l = Math.ceil(_objects.length / 5000) ; i < l; i++) {
-            var canvas = document.createElement("canvas");
-            canvas.width = width;
-            canvas.height = height;
-            _layers.push({
-                ctx: canvas.getContext("2d"),
-                objects: _objects.slice(i * 5000, (i + 1) * 5000)
-            });
-            $("#viewport").append(canvas);
+        $("canvas", "#viewport").remove();
+        var canvas0 = document.createElement("canvas");
+        canvas0.id = "static";
+        var staticLayer = {
+            invalid: true,
+            isStatic: true,
+            ctx: canvas0.getContext("2d"),
+            objects: []
+        };
+        canvas0.width = width;
+        canvas0.height = height;
+        $("#viewport").append(canvas0);
+
+        var canvas1 = document.createElement("canvas");
+        canvas1.id = "dynamic";
+        var dynamicLayer = {
+            isStatic: false,
+            ctx: canvas1.getContext("2d"),
+            objects: []
+        };
+        canvas1.width = width;
+        canvas1.height = height;
+        $("#viewport").append(canvas1);
+
+        for (var i = 0, l = _objects.length; i < l; i++) {
+            var obj = _objects[i];
+            if (obj.invalid) {
+                dynamicLayer.objects.push(obj);
+            } else
+                staticLayer.objects.push(obj);
         }
+        _layers = [staticLayer, dynamicLayer];
     };
 
+    var updateLayers = function () {
+        var staticLayer = _layers[0];
+        var dynamicLayer = _layers[1];
+        for (var j = 0; j < staticLayer.objects.length; j++) {
+            var obj = staticLayer.objects[j];
+            if (obj.invalid) {
+                dynamicLayer.objects.push(obj);
+                staticLayer.objects.splice(j, 1);
+                j--;
+            }
+        }
+        for (var j = 0; j < dynamicLayer.objects.length; j++) {
+            var obj = dynamicLayer.objects[j];
+            if (!obj.invalid) {
+                staticLayer.objects.push(obj);
+                dynamicLayer.objects.splice(j, 1);
+                j--;
+                staticLayer.invalid = true;
+            }
+        }
+    }
     this.invalidate = function () {
-        //var rect = self.getRect();
-        //var modifiedObjects = _objects.filter(function (item) {
-        //    item.setTransformations(ctx, _FPS);
-        //    return item.invalid && item.rectIntersect(rect);
-        //});
-        //updateGrid(_objects);
         for (var i = 0; i < _layers.length; i++) {
             var layer = _layers[i];
+            if (layer.isStatic)
+                if (!layer.invalid)
+                    continue;
+                else
+                    layer.invalid = false;
+
             var ctx = layer.ctx;
             ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height);
-            if (i == 0)
+            if (i == 1)
                 self.refreshFps(ctx);
 
             for (var j = 0, l = layer.objects.length; j < l; j++) {
                 layer.objects[j].setTransformations(ctx, _FPS);
+            }
+            updateLayers();
+            for (var j = 0, l = layer.objects.length; j < l; j++) {
                 layer.objects[j].draw(ctx, _FPS);
             }
         }
-
         eventManager.trigger(_events.onInvalidate, {
             context: ctx,
             fps: _FPS
@@ -103,10 +146,10 @@
             return;
         }
         _objects.push(obj);
-        createLayers();
     };
 
     this.render = function () {
+        createLayers();
         self.invalidate();
     }
 }
